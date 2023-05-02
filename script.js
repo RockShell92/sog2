40558348ab0cb7c394740d4954fe671f83803654
Rachel Mullis <rock.shell92@Macintosh.local>
2023-05-02 10:59:39 -0500

    fixing verbage & sizing and stuff

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 import { Carousel, initTE } from "tw-elements";
 
 initTE({ Carousel });
+
+// Initialization for ES Users
+import { Collapse, Dropdown, initTE } from "tw-elements";
+
+initTE({ Collapse, Dropdown });
